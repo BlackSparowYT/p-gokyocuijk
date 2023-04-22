7ecffa035c8e8d77b1b98a0fd1eb14b792260e3a
BlackSparowYT <1203142@student.roc-nijmegen.nl>
2023-04-22 18:26:21 +0200

    Navbar & Footer blocks
    Remote files for the Navbar & Footer so it is easily editable

---
+++ files/components/footer-home.php
@@ -0,0 +1,58 @@
+<footer>
+    <div class="flex-box-1">
+        <div class="block-1">
+            <img src="./images/footer-logo.png">
+            <h3>Page Version: v1.1</h3>
+            <h3>Site Version: v1.1</h3>
+        </div>
+        <div class="block-2">
+            <div class="block-2-1">
+                <h3>Over deze site:</h3>
+                <p>
+                    Deze site is gemaakt voor de vereniging Gokyo Cuijk, over Gokyo Cuijk kun je meer vinden op onze <a href="./over-ons/index.html">over ons</a> pagina.
+                </p>
+            </div>
+            <div class="block-2-2-v1">
+                <h3>Alle Pagina's:</h3>
+                <div class="block-2-2-1">
+                    <p>- <a href="./index.html">Home</a></p>
+                    <p>- <a href="./informatie/index.html">Informatie</a></p>
+                    <p>-- <a href="https://www.facebook.com/gokyocuijk/" target="_blank">Nieuws</a></p>
+                    <p>-- <a href="./informatie/lestijden/index.html">Les Tijden</a></p>
+                    <p>-- <a href="./informatie/agenda/index.html">Agenda</a></p>
+                    <p>-- <a href="./informatie/huisregels/index.html">Huisregels</a></p>
+                </div>
+                <div class="block-2-2-2">
+                    <p>-- <a href="./informatie/exameneisen/index.html">Examen Eisen</a></p>
+                    <p>-- <a href="./informatie/inschrijven/index.html">Inschrijven</a></p>
+                    <p>-- <a href="./informatie/recent/index.html">Recent</a></p>
+                    <p>- <a href="./contact/index.html">Contact</a></p>
+                    <p>- <a href="./over-ons/index.html">Over ons</a></p>
+                    <p>- <a href="./sponsors/index.html">Sponsors</a></p>
+                </div>
+            </div>
+            <div class="block-2-2-v2">
+                <h3>Alle Pagina's:</h3>
+                <div class="block-2-2-1">
+                    <a href="./index.html">Home</a>
+                    <a href="./informatie/index.html">Informatie</a>
+                    <a href="https://www.facebook.com/gokyocuijk/" target="_blank">Nieuws</a>
+                    <a href="./informatie/lestijden/index.html">Les Tijden</a>
+                    <a href="./informatie/agenda/index.html">Agenda</a>
+                    <a href="./informatie/huisregels/index.html">Huisregels</a>
+                </div>
+                <div class="block-2-2-2">
+                    <a href="./informatie/exameneisen/index.html">Examen Eisen</a>
+                    <a href="./informatie/inschrijven/index.html">Inschrijven</a>
+                    <a href="./contact/index.html">Contact</a>
+                    <a href="./over-ons/index.html">Over ons</a>
+                    <a href="./sponsors/index.html">Sponsors</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <div class="block-3">
+        <h3>&#169;<script>document.write(new Date().getFullYear())</script> - Gokyo Cuijk <!--| <a href="https://design-atlas.nl" target="_blank">Design Atlas</a>--></h3>
+    </div>
+</footer>
---
+++ files/components/navbar-home.php
@@ -0,0 +1,76 @@
+<nav>
+    <div id="navbar-v1">
+        <div class="navbar-v1-sitelogo">
+            <a><img src="./images/navbar-logo.png"></a>
+        </div>
+        <div class="navbar-v1-items">
+            <a href="index.html" class="active hoverbtn"><h3>Home</h3></a>
+            <div class="dropdown">
+                <a onclick="myFunction3()" class="dropbtn hoverbtn"><h3 class="dropbtn">Informatie</h3></a>
+                <div id="dropdownMenu" class="dropdown-content">
+                    <a href="https://www.facebook.com/gokyocuijk/">Nieuws</a>
+                    <a href="./informatie/lestijden/index.html">Les Tijden</a>
+                    <a href="./informatie/agenda/index.html">Agenda</a>
+                    <a href="./informatie/huisregels/index.html">Huisregels</a>
+                    <a href="./informatie/examens/index.html">Examens</a>
+                    <a href="./informatie/inschrijven/index.php#gokyo">Inschrijven Gokyo</a>
+                    <a href="./informatie/inschrijven/index.php#toernooi">Inschrijven Toernooien</a>
+                    <a href="./informatie/protocollen/index.html">Gedragsregels & Protocollen</a>
+                    <a href="./informatie/recent/index.html">Recent</a>
+                </div>
+            </div>
+            <a href="./contact/index.html" class="hoverbtn"><h3>Contact</h3></a>
+            <a href="./over-ons/index.html" class="hoverbtn"><h3>Over Ons</h3></a>
+            <a href="./sponsors/index.html" class="hoverbtn"><h3>Sponsors</h3></a>
+        </div>
+    </div>
+
+    <script>
+        /* When the user clicks on the button, 
+        toggle between hiding and showing the dropdown content */
+        function myFunction3() {
+            document.getElementById("dropdownMenu").classList.toggle("show");
+        }
+        
+        // Close the dropdown if the user clicks outside of it
+        window.onclick = function(event) {
+            if (!event.target.matches('.dropbtn')) {
+            var dropdowns = document.getElementsByClassName("dropdown-content");
+            var i;
+            for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+                }
+            }
+            }
+        }
+    </script>
+
+
+    
+    <div id="navbar-v2">
+        <div class="navbar-v2-sitelogo">
+            <a><img src="./images/navbar-logo.png"></a>
+        </div>
+        <div class="navbar-v2-items">
+            <a onclick="openNav()"><h2>&#9776;</h2></a>
+        </div>
+        <div id="navbar-v2-fullscreen" class="nav-overlay">
+            <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">&times;</a>
+            <div class="nav-overlay-content">
+                <a href="./index.html" class="active"><h3>Home</h3></a>
+                <a href="./informatie/index.html"><h3>Informatie</h3></a>
+                <a href="./contact/index.html"><h3>Contact</h3></a>
+                <a href="./over-ons/index.html"><h3>Over Ons</h3></a>
+                <a href="./sponsors/index.html"><h3>Sponsors</h3></a>
+            </div>
+        </div>
+    </div>
+
+
+    <script>
+        function openNav() { document.getElementById("navbar-v2-fullscreen").style.height = "100%"; }
+        function closeNav() { document.getElementById("navbar-v2-fullscreen").style.height = "0%"; }
+    </script>
+</nav>
