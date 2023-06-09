e9b3b3822d1b699abe0c97a003abe317d86c794d
Black Sparow <117974776+BlackSparowYT@users.noreply.github.com>
2023-06-09 20:26:49 +0200

    move to php folder
    moved all files that are for the php remake to the php folder
    (This is for now ignored in the github commits)

--- informatie/inschrijven.php
+++ informatie/inschrijven.php
@@ -47,44 +47,44 @@
             <h2>Inschrijven Toernooien:</h2>
             <div class="flex-box">
                 
-                    <?php 
+                <?php 
 
-                        // Huidige datum
-                        $date = date("Y/m/d");
+                    // Huidige datum
+                    $date = date("Y/m/d");
 
-                        // Datum dat het inschrijven dicht gaat.
-                        $expDate = "2023/03/24";
+                    // Datum dat het inschrijven dicht gaat.
+                    $expDate = "2023/03/24";
 
-                        // Link van het formulier
-                        $formLink = "https://docs.google.com/forms/d/e/1FAIpQLSeQl_Ha1ir-wVirVezyu1TqDQl-rtXZJXPFodeWnwCH-YpXVg/viewform";
+                    // Link van het formulier
+                    $formLink = "https://docs.google.com/forms/d/e/1FAIpQLSeQl_Ha1ir-wVirVezyu1TqDQl-rtXZJXPFodeWnwCH-YpXVg/viewform";
 
-                        // Informatie van het toernooi
-                        $toerNaam = "Clubkampioenschappen";     // De naam van het toernooi
-                        $toerPlaats = "onze eigen Dojo";        // Waar vind het plaats (zorg dat het in de zin past)
-                        $toerDatum = "09/05/2023";              // De datum in DD/MM/YYYY
+                    // Informatie van het toernooi
+                    $toerNaam = "Clubkampioenschappen";     // De naam van het toernooi
+                    $toerPlaats = "onze eigen Dojo";        // Waar vind het plaats (zorg dat het in de zin past)
+                    $toerDatum = "09/05/2023";              // De datum in DD/MM/YYYY
 
-                        // Kijk of de huidige datum voor de einddatum ligt, ja dan laat het formulier zien, nee laat niks zien.
-                        if (strtotime($date) < strtotime($expDate)) {
-                            echo '
-                                <div class="block block-LT">
-                                    <h3>[Toernooi Naam]:</h3>
-                                    <p>
-                                    Wil jij je inschrijven voor de '.$toerNaam.' doe dat dan hieronder!
-                                    <br>
-                                    Dit toernooi vind zich plaats in '.$toerPlaats.' op '.$toerDatum.'. Inschrijven is mogenlijk tot '.$expDate.'.
-                                    </p>
-                                    <iframe src="'.$formLink.'"></iframe>
-                                    <a class="tele-btn" href="'.$formLink.'">Open formulier</a>
-                                    <p class="help-text" >Laad of werkt het formulier niet? Klik dan <a href="'.$formLink.'" target="_blank">hier</a></p>
-                                </div>';
-                        } else {
-                            echo '';
+                    // Kijk of de huidige datum voor de einddatum ligt, ja dan laat het formulier zien, nee laat niks zien.
+                    if (strtotime($date) < strtotime($expDate)) {
+                        echo '
+                            <div class="block block-LT">
+                                <h3>[Toernooi Naam]:</h3>
+                                <p>
+                                Wil jij je inschrijven voor de '.$toerNaam.' doe dat dan hieronder!
+                                <br>
+                                Dit toernooi vind zich plaats in '.$toerPlaats.' op '.$toerDatum.'. Inschrijven is mogenlijk tot '.$expDate.'.
+                                </p>
+                                <iframe src="'.$formLink.'"></iframe>
+                                <a class="tele-btn" href="'.$formLink.'">Open formulier</a>
+                                <p class="help-text" >Laad of werkt het formulier niet? Klik dan <a href="'.$formLink.'" target="_blank">hier</a></p>
+                            </div>';
+                    } else {
+                        echo '';
 
-                            $geenToernooi = 1;
-                        }
-                        
+                        $geenToernooi = 1;
+                    }
+                    
 
-                    ?>
+                ?>
                 <?php 
 
                     // Huidige datum
